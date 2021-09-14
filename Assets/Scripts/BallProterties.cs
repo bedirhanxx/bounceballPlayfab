855cbc1fcafe400878fb281dfe1320d1bffbad09
bedirhanxx <bedirhanucak@gmail.com>
2021-09-15 02:52:52 +0300

    test

--- Assets/Scripts/BallProterties.cs
+++ Assets/Scripts/BallProterties.cs
@@ -20,6 +20,7 @@ public class BallProterties : MonoBehaviour
     {
         Ball = GameObject.FindGameObjectWithTag("Ball");
         Time.timeScale = 0;
+        InvokeRepeating("getDynamicValues", 5.0f, 5f);
     }
 
 
@@ -85,6 +86,10 @@ public class BallProterties : MonoBehaviour
         setAndGetStat();
     }
 
+
+
+
+
     void setAndGetStat()
     {
         PlayfabController.PFC.sizeX = sizeX;
@@ -101,9 +106,21 @@ public class BallProterties : MonoBehaviour
         Ball.GetComponent<Renderer>().material.color = new Color32((byte)PlayfabController.PFC.redC, (byte)PlayfabController.PFC.greenC, (byte)PlayfabController.PFC.blueC, 255);
 
     }
-    
-    void Update()
+
+    void getDynamicValues()
     {
         
+        PlayfabController.PFC.GetStats();
+        PlayfabController.PFC.SetStats();
+
+        Ball.transform.localScale = new Vector3(PlayfabController.PFC.sizeX, PlayfabController.PFC.sizeY, -1f);
+        Ball.gameObject.GetComponent<Rigidbody2D>().mass = PlayfabController.PFC.rbMass;
+        Ball.GetComponent<Renderer>().material.color = new Color32((byte)PlayfabController.PFC.redC, (byte)PlayfabController.PFC.greenC, (byte)PlayfabController.PFC.blueC, 255);
+
+        
     }
+
+
+
+
 }
